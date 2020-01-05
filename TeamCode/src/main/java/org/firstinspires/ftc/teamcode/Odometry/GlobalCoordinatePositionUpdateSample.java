dcef96bc5ebbcca33d11a866d5fc4a58d8462579
Matthew Duphily <matthew.duphily@live.com>
2020-01-05 14:01:08 -0500

    Fixed right odometer from giving negative value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/GlobalCoordinatePositionUpdateSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/GlobalCoordinatePositionUpdateSample.java
@@ -18,7 +18,7 @@ public class GlobalCoordinatePositionUpdateSample extends LinearOpMode {
     final double COUNTS_PER_INCH = 307.699557;
 
     //Hardware map names for the encoder wheels. Again, these will change for each robot and need to be updated below
-    String verticalLeftEncoderName = "blMotor", verticalRightEncoderName = "blMotor", horizontalEncoderName = "frMotor";
+    String verticalLeftEncoderName = "blMotor", verticalRightEncoderName = "brMotor", horizontalEncoderName = "frMotor";
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -32,6 +32,8 @@ public class GlobalCoordinatePositionUpdateSample extends LinearOpMode {
         verticalRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         verticalLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         horizontal.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        verticalRight.setDirection(DcMotor.Direction.REVERSE);
 
         /*
         Reverse the direction of the odometry wheels. THIS WILL CHANGE FOR EACH ROBOT. Adjust the direction (as needed) of each encoder wheel
@@ -65,7 +67,7 @@ public class GlobalCoordinatePositionUpdateSample extends LinearOpMode {
         positionThread.start();
 
         //Use this if encoder values are wrong
-//        globalPositionUpdate.reverseLeftEncoder();
+//        globalPositionUpdate.reverseRightEncoder();
 
         while(opModeIsActive()){
             //Display Global (x, y, theta) coordinates
